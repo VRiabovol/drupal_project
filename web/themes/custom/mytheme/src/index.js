6c23d59b9bcb489fa9aa47e2428cc187686852d7
VladRiabovol <82464669+VladRiabovol@users.noreply.github.com>
2022-07-22 17:01:10 +0300

    #5109: theming news slider block, edit font menu category block

--- web/themes/custom/mytheme/src/index.js
+++ web/themes/custom/mytheme/src/index.js
@@ -3,6 +3,7 @@ scss
  */
 import "./scss/Global.scss";
 import "./scss/Header.scss";
+import "./scss/region1.scss";
 // import "./scss/sidebar.scss";
 // import "./scss/FeaturedTop.scss";
 // import "./scss/front_views.scss";
