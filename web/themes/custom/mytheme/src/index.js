171aeedd35a7b0a3f0456f6b62bb40e079070fa1
VladRiabovol <82464669+VladRiabovol@users.noreply.github.com>
2022-07-29 11:27:17 +0300

    #5177: edit block layout, edit views, add image styles

--- web/themes/custom/mytheme/src/index.js
+++ web/themes/custom/mytheme/src/index.js
@@ -7,6 +7,7 @@ import "./scss/region2.scss";
 import "./scss/region3.scss";
 import "./scss/region4.scss";
 import "./scss/region5.scss";
+import "./scss/region7.scss";
 import "./scss/social_media.scss";
 // import "./scss/sidebar.scss";
 // import "./scss/FeaturedTop.scss";
